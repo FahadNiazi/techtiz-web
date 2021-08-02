d8665a2aa0f000ec5e3e6533e7960b6afe047a56
Fahad niazi <fahadniazi142@gmail.com>
2021-08-02 19:18:04 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Introduction } from "./components/Introduction";
+import { OverView } from "./components/OverView";
+import { OurServices } from "../src/components/ourServices";
+import { Card } from "@material-ui/core";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Introduction />
+      <OverView />
+      <OurServices />
     </div>
   );
 }
